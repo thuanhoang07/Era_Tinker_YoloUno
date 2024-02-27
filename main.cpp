149a3f533038ead2759a8c29d172a0401042ae1e
thuanhoang07 <116453144+thuanhoang07@users.noreply.github.com>
2024-02-27 12:26:27 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -49,8 +49,8 @@ ERA_DISCONNECTED() {
 void timerEvent() {
     console.requestHumidity(100);
     console.requestTemperature(100);
-    console.requestGas(100);
-    console.requestFlame(100);
+    console.requestGas(70);
+    console.requestFlame(30);
     printf("Uptime: %d\r\n", ERaMillis() / 1000L);
 }
 
