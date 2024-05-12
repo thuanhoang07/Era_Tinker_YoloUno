00cbf4a7119807a96b5a4658e208a0bb7da3cd47
thuanhoang07 <116453144+thuanhoang07@users.noreply.github.com>
2024-05-12 18:03:32 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ static const char* user;
 static const char* pass;
 
 static int devBaud = 115200;
-static const char* devName = "/dev/serial/by-id/usb-Unexpected_Maker_TinyS3__ts3_-if00";
+static const char* devName = "/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0";
 
 ERaSerialLinux serial;
 ERaConsole console(serial);
